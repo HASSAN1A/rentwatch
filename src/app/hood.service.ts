5de337911798ad096662df057d15633caf2efe42
Hassan Juma <okothhassanjuma@gmail.com>
2021-05-13 18:00:03 +0300

    second commit

---
+++ src/app/hood.service.ts
@@ -0,0 +1,19 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HoodService {
+
+  baseurl = "https://hood256.herokuapp.com/";
+
+
+  constructor(private http: HttpClient) { }
+
+  getAllhoods(): Observable<any> {
+    return this.http.get(this.baseurl + '/hoods/');
+  }
+}
